916e5e2e9379818d56902fe747c08ca235a2ca34
DesireeCost4 <desireec.dev@gmail.com>
2024-02-23 15:50:43 -0300

    Subindo Projeto

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import{pokemonData} from '../models/pokemonData'
+
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private baseURL:string=""
+  private pokeData: pokemonData | any
+
+  constructor(private http:HttpClient)
+    {
+      this.baseURL = environment.pokeApi
+
+    }
+
+  getPokemon(pokemoName:string): Observable <pokemonData> {
+   this.pokeData = this.http.get<pokemonData>(`${this.baseURL}${pokemoName}`)
+    return this.pokeData
+  }
+}
